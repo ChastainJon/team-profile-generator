13d16ca1caa954efe4ba7b604d7e2ec014e0d7b3
Jon Chastain <chastain.r.jon@gmail.com>
2022-01-14 23:16:53 -0500

    Added questions for user input

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
 const inquirer = require('inquirer')
+
+const employees = []
+
+const promptUser = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What role is this employee?',
+                choices: ['Manager', 'Engineer', 'Intern']
+                
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your user ID?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your e-mail'
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is your office number?',
+                when: ({role}) => {
+                    if(role === 'Manager'){
+                        return true
+                    }else{
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'githubUsername',
+                message: 'What is your GitHub username?',
+                when: ({role}) => {
+                    if(role === 'Engineer'){
+                        return true
+                    }else{
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school do you go to?',
+                when: ({role}) => {
+                    if(role === 'Intern'){
+                        return true
+                    }else{
+                        return false
+                    }
+
+                }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmNewEmployee',
+                message: 'Would you like to enter another employee?',
+                default: false
+            }
+
+        ]).then(employeeData =>{
+            employees.push(employeeData)
+            if(employeeData.confirmNewEmployee){
+                return (promptUser(employeeData))
+            }else{
+                return employeeData
+            }
+        })
+}
+
+promptUser()
+.then(() =>{
+    console.log(employees)
+})
